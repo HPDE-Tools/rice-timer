fe33ba9310d11cc035e68124a613c659cb4c4b06
summivox <summivox@github.com>
2021-02-07 13:07:14 -0800

    button up Task class using atomic task handle

--- main/common/task.hpp
+++ main/common/task.hpp
@@ -3,33 +3,43 @@
 
 #pragma once
 
+#include <atomic>
+
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 class Task {
  public:
-  TaskHandle_t handle() const { return handle_; }
-  virtual ~Task() = default;
+  TaskHandle_t handle() const { return handle_.load(); }
+  virtual ~Task() { Kill(); }
 
  protected:
-  TaskHandle_t handle_ = nullptr;
+  static_assert(std::atomic<TaskHandle_t>::is_always_lock_free);
 
   explicit Task() {}
 
   virtual void Run() = 0;
 
   esp_err_t Spawn(const char* name, uint32_t stack_depth, uint32_t priority) {
-    return xTaskCreate(RunAdapter, name, stack_depth, this, priority, &handle_) == pdTRUE
-               ? ESP_OK
-               : ESP_ERR_NO_MEM;
+    Kill();
+    TaskHandle_t handle = nullptr;
+    if (xTaskCreate(RunAdapter, name, stack_depth, this, priority, &handle) != pdTRUE) {
+      return ESP_ERR_NO_MEM;
+    }
+    handle_.store(handle);
+    return ESP_OK;
   }
 
   esp_err_t SpawnPinned(const char* name, uint32_t stack_depth, uint32_t priority, BaseType_t cpu) {
-    return xTaskCreatePinnedToCore(RunAdapter, name, stack_depth, this, priority, &handle_, cpu) ==
-                   pdTRUE
-               ? ESP_OK
-               : ESP_ERR_NO_MEM;
+    Kill();
+    TaskHandle_t handle = nullptr;
+    if (xTaskCreatePinnedToCore(RunAdapter, name, stack_depth, this, priority, &handle, cpu) !=
+        pdTRUE) {
+      return ESP_ERR_NO_MEM;
+    }
+    handle_.store(handle);
+    return ESP_OK;
   }
 
   esp_err_t SpawnSame(const char* name, uint32_t stack_depth, uint32_t priority) {
@@ -37,13 +47,15 @@ class Task {
   }
 
   void Kill() {
-    if (handle_) {
-      vTaskDelete(handle_);
-      handle_ = nullptr;
+    const TaskHandle_t handle = handle_.exchange(nullptr);
+    if (handle) {
+      vTaskDelete(handle);
     }
   }
 
  private:
+  std::atomic<TaskHandle_t> handle_ = nullptr;
+
   Task(const Task&) = delete;
   Task(Task&&) = delete;
   Task& operator=(const Task&) = delete;
