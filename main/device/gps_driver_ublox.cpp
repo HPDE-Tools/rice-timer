571cbc7ea0087b30affb75cf7520f6404e6eb7e0
summivox <summivox@github.com>
2021-06-20 17:58:15 -0700

    fully configure uBlox GPS thru I2C and save to battery-backed RAM

--- main/device/gps_driver_ublox.cpp
+++ main/device/gps_driver_ublox.cpp
@@ -3,113 +3,174 @@
 
 #include "device/gps_driver_ublox.hpp"
 
-#include <utility>
-#include <vector>
+#include <algorithm>
 
+#include "common/i2c_helpers.hpp"
 #include "common/logging.hpp"
-#include "common/scope_guard.hpp"
 #include "device/gps_utils.hpp"
-#include "io/autobaud.hpp"
-
-#define UINT16_LE_BYTES(x) uint8_t(x), uint8_t(((x) >> 8))
-#define UINT32_LE_BYTES(x) uint8_t(x), uint8_t((x) >> 8), uint8_t((x) >> 16), uint8_t((x) >> 24)
 
 namespace {
+constexpr char TAG[] = "ubx";
 
-constexpr uint8_t kUbxSync1 = 0xB5;
-constexpr uint8_t kUbxSync2 = 0x62;
-
-struct UbxFrame {
-  union {
-    struct {
-      uint8_t class_id;
-      uint8_t message_id;
-      mutable uint16_t len;
-    } header;
-    uint8_t header_bytes[4];
-  };
-  std::vector<uint8_t> payload;
-
-  std::pair<uint8_t, uint8_t> GetChecksum() const {
-    uint8_t a{};
-    uint8_t b{};
-    header.len = payload.size();
-    for (const uint8_t x : header_bytes) {
-      a += x;
-      b += a;
+template <typename Lambda>
+esp_err_t DelayRetry(
+    int max_num_retry, TickType_t init_delay, TickType_t max_delay, Lambda lambda) {
+  esp_err_t result{};
+  TickType_t delay = init_delay;
+  for (int i = 0; i < max_num_retry; i++) {
+    result = lambda();
+    if (result == ESP_OK) {
+      return result;
     }
-    for (const uint8_t x : payload) {
-      a += x;
-      b += a;
-    }
-    return {a, b};
+    vTaskDelay(delay);
+    delay = std::min(delay * 3 / 2, max_delay);
   }
-};
-
-esp_err_t UbxI2cSendFrame(i2c_port_t i2c_num, uint8_t i2c_address_7bit, const UbxFrame& frame) {
-  i2c_cmd_handle_t txn = i2c_cmd_link_create();
-
-  i2c_master_start(txn);
-  i2c_master_write_byte(txn, (i2c_address_7bit << 1) | I2C_MASTER_WRITE, /*ack*/ true);
-  i2c_master_write_byte(txn, 0xFF, /*ack*/ true);
-
-  const auto [a, b] = frame.GetChecksum();
-
-  i2c_master_start(txn);
-  i2c_master_write_byte(txn, (i2c_address_7bit << 1) | I2C_MASTER_WRITE, /*ack*/ true);
-  i2c_master_write_byte(txn, kUbxSync1, /*ack*/ true);
-  i2c_master_write_byte(txn, kUbxSync2, /*ack*/ true);
-  i2c_master_write(txn, frame.header_bytes, 4, /*ack*/ true);
-  i2c_master_write(txn, frame.payload.data(), frame.payload.size(), /*ack*/ true);
-
-  i2c_master_write_byte(txn, a, /*ack*/ true);
-  i2c_master_write_byte(txn, b, /*ack*/ true);
-
-  i2c_master_stop(txn);
-  return i2c_master_cmd_begin(i2c_num, txn, portMAX_DELAY);
-}
-
-std::optional<UbxFrame> UbxI2cReceiveOneFrame(i2c_port_t i2c_num, uint8_t i2c_address_7bit) {
-  std::optional<UbxFrame> result{};
-  i2c_cmd_handle_t txn = i2c_cmd_link_create();
-  i2c_master_start(txn);
-  i2c_master_write_byte(txn, (i2c_address_7bit << 1) | I2C_MASTER_WRITE, /*ack*/ true);
-  i2c_master_write_byte(txn, 0xFD, /*ack*/ true);
-  i2c_master_start(txn);
-  i2c_master_write_byte(txn, (i2c_address_7bit << 1) | I2C_MASTER_READ, /*ack*/ true);
-  uint8_t len_raw[2];
-  // TODO
-
   return result;
 }
 
-};  // namespace
+}  // namespace
+
+UbxFrame::UbxFrame(uint8_t class_id, uint8_t message_id, uint16_t len)
+    : header_{class_id, message_id, len} {
+  payload_ = std::make_unique<uint8_t[]>(len);
+}
+
+UbxFrame::UbxFrame(uint8_t class_id, uint8_t message_id, std::initializer_list<uint8_t> payload) {
+  CHECK(payload.size() <= UINT16_MAX);
+  header_.class_id = class_id;
+  header_.message_id = message_id;
+  header_.len = payload.size();
+  payload_ = std::make_unique<uint8_t[]>(payload.size());
+  std::copy(payload.begin(), payload.end(), payload_.get());
+}
+
+std::pair<uint8_t, uint8_t> UbxFrame::GetChecksum() const {
+  uint8_t a{};
+  uint8_t b{};
+  for (const uint8_t x : header_bytes_) {
+    a += x;
+    b += a;
+  }
+  for (int i = 0; i < header_.len; i++) {
+    a += payload_[i];
+    b += a;
+  }
+  return {a, b};
+}
+
+esp_err_t UbxI2cSendFrame(i2c_port_t i2c_num, uint8_t i2c_address_7bit, const UbxFrame& frame) {
+  const auto [a, b] = frame.GetChecksum();
+  return I2cTxn()
+      .StartWrite(i2c_address_7bit)
+      .WriteByte(kUbxI2cRegData)
+      .StartWrite(i2c_address_7bit)
+      .WriteByte(kUbxSync1)
+      .WriteByte(kUbxSync2)
+      .Write(frame.header_bytes().data(), 4)
+      .Write(frame.payload(), frame.len())
+      .WriteByte(a)
+      .WriteByte(b)
+      .Stop()
+      .SendTo(i2c_num);
+}
+
+std::optional<uint16_t> UbxI2cGetNumBytesAvailable(i2c_port_t i2c_num, uint8_t i2c_address_7bit) {
+  uint8_t result[2];
+  OK_OR_RETURN(
+      I2cTxn()
+          .StartWrite(i2c_address_7bit)
+          .WriteByte(kUbxI2cRegLen)
+          .Start()
+          .StartRead(i2c_address_7bit)
+          .Read(result, 2)
+          .Stop()
+          .SendTo(i2c_num),
+      {});
+  return Uint16LeAt(result);
+}
 
 esp_err_t SetupUbloxGpsI2c(
-    i2c_port_t i2c_num, uint8_t i2c_address_7bit, int desired_baud, int desired_output_period_ms) {
-  const UbxFrame ubx_cfg_port = {
-      .header = {.class_id = 0x06, .message_id = 0x00},
-      .payload = {
-          1,                              // portID = UART1
-          0,                              // reserved1
-          UINT16_LE_BYTES(0),             // txReady
-          UINT32_LE_BYTES(0x08C0),        // mode = 8N1
-          UINT32_LE_BYTES(desired_baud),  // baud
-          UINT16_LE_BYTES(0b000011),      // inProtoMask = NMEA | UBX
-          UINT16_LE_BYTES(0b000010),      // outProtoMask = NMEA
-          UINT16_LE_BYTES(0x0002),        // flags
-          UINT16_LE_BYTES(0),             // reserved2
-      }};
-  TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_port));
+    i2c_port_t i2c_num,
+    uint8_t i2c_address_7bit,
+    int desired_uart_baud,
+    int desired_output_period_ms) {
+  const UbxFrame ubx_cfg_port_i2c{
+      0x06,  // CFG
+      0x00,  // CFG-PORT
+      {
+          0,                                       // portID = DDC (I2C)
+          0,                                       // reserved1
+          UINT16_LE_BYTES(0),                      // txReady
+          UINT32_LE_BYTES(i2c_address_7bit << 1),  // mode = keep current slave addr
+          UINT32_LE_BYTES(0),                      // reserved2
+          UINT16_LE_BYTES(0b000011),               // inProtoMask = NMEA | UBX
+          UINT16_LE_BYTES(0b000001),               // outProtoMask = UBX
+          UINT16_LE_BYTES(0x0002),                 // flags = extendedTxTimeout
+          UINT16_LE_BYTES(0),                      // reserved3
+      },
+  };
+  TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_port_i2c));
 
-  const UbxFrame ubx_cfg_rate = {
-      .header = {.class_id = 0x06, .message_id = 0x08},
-      .payload = {
+  const UbxFrame ubx_cfg_port_uart1{
+      0x06,  // CFG
+      0x00,  // CFG-PORT
+      {
+          1,                                   // portID = UART1
+          0,                                   // reserved1
+          UINT16_LE_BYTES(0),                  // txReady
+          UINT32_LE_BYTES(0x08C0),             // mode = 8N1
+          UINT32_LE_BYTES(desired_uart_baud),  // baud
+          UINT16_LE_BYTES(0b000011),           // inProtoMask = NMEA | UBX
+          UINT16_LE_BYTES(0b000010),           // outProtoMask = NMEA
+          UINT16_LE_BYTES(0x0002),             // flags = extendedTxTimeout
+          UINT16_LE_BYTES(0),                  // reserved2
+      },
+  };
+  TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_port_uart1));
+
+  for (const auto [mnemonic, cls, id, rate] : kNmeaMessages) {
+    ESP_LOGI(TAG, "%c%c%c%c = %d", UINT32_BE_BYTES(mnemonic), rate);
+    const UbxFrame ubx_cfg_msg{
+        0x06,  // CFG
+        0x01,  // CFG_MSG
+        {
+            cls,
+            id,
+            0,     // 0 -- I2C
+            rate,  // 1 -- UART1
+            0,     // 2
+            1,     // 3 -- USB
+            0,     // 4 -- SPI
+            0,     // 5
+        },
+    };
+    TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_msg));
+  }
+
+  // configure output rate
+  const UbxFrame ubx_cfg_rate{
+      0x06,  // CFG
+      0x08,  // CFG_RATE
+      {
           UINT16_LE_BYTES(desired_output_period_ms),  // measRate
           UINT16_LE_BYTES(1),                         // navRate (divider)
           UINT16_LE_BYTES(1),                         // timeRef
-      }};
+      },
+  };
   TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_rate));
+
+  // save to battery-backed RAM
+  const UbxFrame ubx_cfg_cfg_save{
+      0x06,  // CFG
+      0x09,  // CFG_CFG
+      {
+          UINT32_LE_BYTES(0),           // clearMask
+          UINT32_LE_BYTES(0x00001F1F),  // saveMask
+          UINT32_LE_BYTES(0),           // loadMask
+          0b0000'0001,                  // deviceMask = devBBR
+      },
+  };
+  TRY(UbxI2cSendFrame(i2c_num, i2c_address_7bit, ubx_cfg_cfg_save));
 
   return ESP_OK;
 }
--- main/device/gps_driver_ublox.hpp
+++ main/device/gps_driver_ublox.hpp
@@ -3,13 +3,83 @@
 
 #pragma once
 
-#include "esp_err.h"
+#include <array>
+#include <memory>
+#include <optional>
+#include <utility>
 
 #include "driver/i2c.h"
 #include "driver/uart.h"
+#include "esp_err.h"
 #include "soc/uart_struct.h"
 
 #include "io/uart_line_reader.hpp"
 
+constexpr uint8_t kUbxSync1 = 0xB5;
+constexpr uint8_t kUbxSync2 = 0x62;
+constexpr uint8_t kUbxI2cRegLen = 0xFD;
+constexpr uint8_t kUbxI2cRegData = 0xFF;
+
+// mnemonic, cls, id, rate
+constexpr std::tuple<uint32_t, uint8_t, uint8_t, uint8_t> kNmeaMessages[] = {
+    {uint32_t('DTM '), 0xF0, 0x0A, 0},
+    {uint32_t('GBQ '), 0xF0, 0x44, 0},
+    {uint32_t('GBS '), 0xF0, 0x09, 0},
+    {uint32_t('GGA '), 0xF0, 0x00, 1},
+    {uint32_t('GLL '), 0xF0, 0x01, 0},
+    {uint32_t('GLQ '), 0xF0, 0x43, 0},
+    {uint32_t('GNQ '), 0xF0, 0x42, 0},
+    {uint32_t('GNS '), 0xF0, 0x0D, 0},
+    {uint32_t('GPQ '), 0xF0, 0x40, 0},
+    {uint32_t('GRS '), 0xF0, 0x06, 0},
+    {uint32_t('GSA '), 0xF0, 0x02, 0},
+    {uint32_t('GST '), 0xF0, 0x07, 1},
+    {uint32_t('GSV '), 0xF0, 0x03, 0},
+    {uint32_t('RMC '), 0xF0, 0x04, 1},
+    {uint32_t('TXT '), 0xF0, 0x41, 0},
+    {uint32_t('VLW '), 0xF0, 0x0F, 0},
+    {uint32_t('VTG '), 0xF0, 0x05, 0},
+    {uint32_t('ZDA '), 0xF0, 0x08, 0},
+};
+
+struct UbxHeader {
+  uint8_t class_id;
+  uint8_t message_id;
+  uint16_t len;
+};
+static_assert(sizeof(UbxHeader) == 4);
+
+class UbxFrame {
+ public:
+  /// constructs empty frame of known length
+  UbxFrame(uint8_t class_id, uint8_t message_id, uint16_t len);
+
+  /// constructs frame from brace list
+  UbxFrame(uint8_t class_id, uint8_t message_id, std::initializer_list<uint8_t> payload);
+
+  UbxHeader header() const { return header_; }
+  const std::array<uint8_t, 4>& header_bytes() const { return header_bytes_; }
+  uint8_t class_id() const { return header_.class_id; }
+  uint8_t message_id() const { return header_.message_id; }
+  uint16_t len() const { return header_.len; }
+  uint8_t* payload() const { return payload_.get(); }
+
+  std::pair<uint8_t, uint8_t> GetChecksum() const;
+
+ private:
+  // NOTE: ESP32 is little endian --- consistent with UBX protocol
+  union {
+    UbxHeader header_;
+    std::array<uint8_t, 4> header_bytes_;
+  };
+  std::unique_ptr<uint8_t[]> payload_;
+};
+
+esp_err_t UbxI2cSendFrame(i2c_port_t i2c_num, uint8_t i2c_address_7bit, const UbxFrame& frame);
+std::optional<uint16_t> UbxI2cGetNumBytesAvailable(i2c_port_t i2c_num, uint8_t i2c_address_7bit);
+
 esp_err_t SetupUbloxGpsI2c(
-    i2c_port_t i2c_num, uint8_t i2c_address_7bit, int desired_baud, int desired_output_period_ms);
+    i2c_port_t i2c_num,
+    uint8_t i2c_address_7bit,
+    int desired_uart_baud,
+    int desired_output_period_ms);
