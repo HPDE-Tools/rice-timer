4701c7427c87a5668898022ddda5fac330894f6b
summivox <summivox@gmail.com>
2021-07-14 01:51:41 -0700

    port spinlock for ringbuf (using esp_rom_delay_us for now)

--- components/ringbuf/ringbuf_c/utils.h
+++ components/ringbuf/ringbuf_c/utils.h
@@ -41,9 +41,9 @@
  * A regular assert (debug/diagnostic only).
  */
 #if defined(DEBUG)
-#define	ASSERT		assert
+#define ASSERT assert
 #else
-#define	ASSERT(x)
+#define ASSERT(x)
 #endif
 
 /*
@@ -51,19 +51,19 @@
  */
 
 #ifndef MIN
-#define	MIN(x, y)	((x) < (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 #endif
 
 #ifndef MAX
-#define	MAX(x, y)	((x) > (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 #endif
 
 /*
  * Branch prediction macros.
  */
 #ifndef __predict_true
-#define	__predict_true(x)	__builtin_expect((x) != 0, 1)
-#define	__predict_false(x)	__builtin_expect((x) != 0, 0)
+#define __predict_true(x) __builtin_expect((x) != 0, 1)
+#define __predict_false(x) __builtin_expect((x) != 0, 0)
 #endif
 
 /*
@@ -75,41 +75,43 @@
  * that here.
  */
 #ifndef atomic_compare_exchange_weak
-#define	atomic_compare_exchange_weak(ptr, expected, desired) \
-    __sync_bool_compare_and_swap(ptr, *(expected), desired)
+#define atomic_compare_exchange_weak(ptr, expected, desired) \
+  __sync_bool_compare_and_swap(ptr, *(expected), desired)
 #endif
 
 #ifndef atomic_thread_fence
-#define	memory_order_relaxed	__ATOMIC_RELAXED
-#define	memory_order_acquire	__ATOMIC_ACQUIRE
-#define	memory_order_release	__ATOMIC_RELEASE
-#define	memory_order_seq_cst	__ATOMIC_SEQ_CST
-#define	atomic_thread_fence(m)	__atomic_thread_fence(m)
+#define memory_order_relaxed __ATOMIC_RELAXED
+#define memory_order_acquire __ATOMIC_ACQUIRE
+#define memory_order_release __ATOMIC_RELEASE
+#define memory_order_seq_cst __ATOMIC_SEQ_CST
+#define atomic_thread_fence(m) __atomic_thread_fence(m)
 #endif
 #ifndef atomic_store_explicit
-#define	atomic_store_explicit	__atomic_store_n
+#define atomic_store_explicit __atomic_store_n
 #endif
 #ifndef atomic_load_explicit
-#define	atomic_load_explicit	__atomic_load_n
+#define atomic_load_explicit __atomic_load_n
 #endif
 
 /*
  * Exponential back-off for the spinning paths.
  */
-#define	SPINLOCK_BACKOFF_MIN	4
-#define	SPINLOCK_BACKOFF_MAX	128
+#define SPINLOCK_BACKOFF_MIN 4
+#define SPINLOCK_BACKOFF_MAX 128
 #if defined(__x86_64__) || defined(__i386__)
-#define SPINLOCK_BACKOFF_HOOK	__asm volatile("pause" ::: "memory")
+#define SPINLOCK_BACKOFF_HOOK __asm volatile("pause" ::: "memory")
+#elif __has_include("esp_rom_sys.h")
+#include "esp_rom_sys.h"
+#define SPINLOCK_BACKOFF_HOOK esp_rom_delay_us(1)
 #else
 #define SPINLOCK_BACKOFF_HOOK
 #endif
-#define	SPINLOCK_BACKOFF(count)					\
-do {								\
-	for (int __i = (count); __i != 0; __i--) {		\
-		SPINLOCK_BACKOFF_HOOK;				\
-	}							\
-	if ((count) < SPINLOCK_BACKOFF_MAX)			\
-		(count) += (count);				\
-} while (/* CONSTCOND */ 0);
+#define SPINLOCK_BACKOFF(count)                             \
+  do {                                                      \
+    for (int __i = (count); __i != 0; __i--) {              \
+      SPINLOCK_BACKOFF_HOOK;                                \
+    }                                                       \
+    if ((count) < SPINLOCK_BACKOFF_MAX) (count) += (count); \
+  } while (/* CONSTCOND */ 0);
 
 #endif
