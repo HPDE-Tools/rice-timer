6851766d4d4cffa2075bc920764d82c1d922980b
summivox <summivox@gmail.com>
2021-07-22 01:21:10 -0700

    update utils to match boilerplate project

--- main/common/utils.hpp
+++ main/common/utils.hpp
@@ -11,6 +11,8 @@
 constexpr char HexDigitLower(uint8_t i) { return "0123456789abcdef"[i & uint8_t{0xf}]; }
 constexpr char HexDigitUpper(uint8_t i) { return "0123456789ABCDEF"[i & uint8_t{0xf}]; }
 
+/// Explicitly return the signed (mod 2^n) difference between unsigned integers. Useful for e.g.
+/// calculating time durations from FreeRTOS `TickType_t`.
 template <typename T>
 constexpr auto SignedMinus(T a, T b) -> std::make_signed_t<T> {
   return static_cast<std::make_signed_t<T>>(a - b);
@@ -51,11 +53,39 @@ constexpr uint32_t Uint32BeAt(const uint8_t* bytes) {
       (static_cast<uint32_t>(bytes[2]) << uint32_t{8}) |
       (static_cast<uint32_t>(bytes[3]) << uint32_t{0}));
 }
+constexpr uint64_t Uint64LeAt(const uint8_t* bytes) {
+  return static_cast<uint64_t>(
+      (static_cast<uint64_t>(bytes[0]) << uint64_t{0}) |
+      (static_cast<uint64_t>(bytes[1]) << uint64_t{8}) |
+      (static_cast<uint64_t>(bytes[2]) << uint64_t{16}) |
+      (static_cast<uint64_t>(bytes[3]) << uint64_t{24}) |
+      (static_cast<uint64_t>(bytes[4]) << uint64_t{32}) |
+      (static_cast<uint64_t>(bytes[5]) << uint64_t{40}) |
+      (static_cast<uint64_t>(bytes[6]) << uint64_t{48}) |
+      (static_cast<uint64_t>(bytes[7]) << uint64_t{56}));
+}
+constexpr uint64_t Uint64BeAt(const uint8_t* bytes) {
+  return static_cast<uint64_t>(
+      (static_cast<uint64_t>(bytes[0]) << uint64_t{56}) |
+      (static_cast<uint64_t>(bytes[1]) << uint64_t{48}) |
+      (static_cast<uint64_t>(bytes[2]) << uint64_t{40}) |
+      (static_cast<uint64_t>(bytes[3]) << uint64_t{32}) |
+      (static_cast<uint64_t>(bytes[4]) << uint64_t{24}) |
+      (static_cast<uint64_t>(bytes[5]) << uint64_t{16}) |
+      (static_cast<uint64_t>(bytes[6]) << uint64_t{8}) |
+      (static_cast<uint64_t>(bytes[7]) << uint64_t{0}));
+}
 
 static_assert(Uint16LeAt(std::array<uint8_t, 2>{0x12u, 0x34u}.data()) == 0x3412u);
 static_assert(Uint16BeAt(std::array<uint8_t, 2>{0x12u, 0x34u}.data()) == 0x1234u);
 static_assert(Uint32LeAt(std::array<uint8_t, 4>{0x12u, 0x34u, 0x56u, 0x78u}.data()) == 0x78563412u);
 static_assert(Uint32BeAt(std::array<uint8_t, 4>{0x12u, 0x34u, 0x56u, 0x78u}.data()) == 0x12345678u);
+static_assert(
+    Uint64LeAt(std::array<uint8_t, 8>{0x12u, 0x34u, 0x56u, 0x78u, 0x9A, 0xBC, 0xDE, 0xF0}.data()) ==
+    0xF0DEBC9A78563412ull);
+static_assert(
+    Uint64BeAt(std::array<uint8_t, 8>{0x12u, 0x34u, 0x56u, 0x78u, 0x9A, 0xBC, 0xDE, 0xF0}.data()) ==
+    0x123456789ABCDEF0ull);
 
 constexpr int16_t Sint16LeAt(const uint8_t* bytes) {
   return static_cast<int16_t>(Uint16LeAt(bytes));
@@ -69,23 +99,40 @@ constexpr int32_t Sint32LeAt(const uint8_t* bytes) {
 constexpr int32_t Sint32BeAt(const uint8_t* bytes) {
   return static_cast<int32_t>(Uint32BeAt(bytes));
 }
+constexpr int64_t Sint64LeAt(const uint8_t* bytes) {
+  return static_cast<int64_t>(Uint64LeAt(bytes));
+}
+constexpr int64_t Sint64BeAt(const uint8_t* bytes) {
+  return static_cast<int64_t>(Uint64BeAt(bytes));
+}
 
 /// comma-separated little-endian bytes of a 16-bit integer (no trailing comma)
 #define UINT16_LE_BYTES(x) uint8_t(x), uint8_t(((x) >> 8))
 /// comma-separated little-endian bytes of a 32-bit integer (no trailing comma)
 #define UINT32_LE_BYTES(x) uint8_t(x), uint8_t((x) >> 8), uint8_t((x) >> 16), uint8_t((x) >> 24)
+/// comma-separated little-endian bytes of a 64-bit integer (no trailing comma)
+#define UINT64_LE_BYTES(x)                                                                   \
+  uint8_t(x), uint8_t((x) >> 8), uint8_t((x) >> 16), uint8_t((x) >> 24), uint8_t((x) >> 32), \
+      uint8_t((x) >> 40), uint8_t((x) >> 48), uint8_t((x) >> 56)
 
 /// comma-separated big-endian bytes of a 16-bit integer (no trailing comma)
 #define UINT16_BE_BYTES(x) uint8_t(((x) >> 8)), uint8_t(x)
 /// comma-separated big-endian bytes of a 32-bit integer (no trailing comma)
 #define UINT32_BE_BYTES(x) uint8_t((x) >> 24), uint8_t((x) >> 16), uint8_t((x) >> 8), uint8_t(x)
+/// comma-separated big-endian bytes of a 64-bit integer (no trailing comma)
+#define UINT64_BE_BYTES(x)                                                        \
+  uint8_t((x) >> 56), uint8_t((x) >> 48), uint8_t((x) >> 40), uint8_t((x) >> 32), \
+      uint8_t((x) >> 24), uint8_t((x) >> 16), uint8_t((x) >> 8), uint8_t(x)
 
-static_assert(Uint16LeAt(std::array<uint8_t, 2>{UINT16_LE_BYTES(0x1234)}.data()) == 0x1234u);
-static_assert(Uint16BeAt(std::array<uint8_t, 2>{UINT16_BE_BYTES(0x1234)}.data()) == 0x1234u);
+static_assert(Uint16LeAt(std::array<uint8_t, 2>{UINT16_LE_BYTES(0x1234u)}.data()) == 0x1234u);
+static_assert(Uint16BeAt(std::array<uint8_t, 2>{UINT16_BE_BYTES(0x1234u)}.data()) == 0x1234u);
 static_assert(
-    Uint32LeAt(std::array<uint8_t, 4>{UINT32_LE_BYTES(0x12345678)}.data()) == 0x12345678u);
+    Uint32LeAt(std::array<uint8_t, 4>{UINT32_LE_BYTES(0x12345678u)}.data()) == 0x12345678u);
 static_assert(
-    Uint32BeAt(std::array<uint8_t, 4>{UINT32_BE_BYTES(0x12345678)}.data()) == 0x12345678u);
+    Uint32BeAt(std::array<uint8_t, 4>{UINT32_BE_BYTES(0x12345678u)}.data()) == 0x12345678u);
+static_assert(
+    Uint64BeAt(std::array<uint8_t, 8>{UINT64_BE_BYTES(0x123456789ABCDEF0ull)}.data()) ==
+    0x123456789ABCDEF0ull);
 
 // http://shimpossible.blogspot.com/2013/08/containerof-and-offsetof-in-c.html
 
