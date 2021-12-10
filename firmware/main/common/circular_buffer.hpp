b81f338fd1d75b782e6f3c978299ca3f43202677
summivox <summivox@gmail.com>
2021-12-09 19:04:13 -0800

    add simple circular buffer impl (tests don't work yet; need build system support)

---
+++ firmware/main/common/circular_buffer.hpp
@@ -0,0 +1,114 @@
+// Copyright 2021 summivox. All rights reserved.
+// Authors: summivox@gmail.com
+
+#include <memory>
+
+#include "fmt/core.h"
+
+#include "common/macros.hpp"
+#include "common/polyfill.hpp"
+
+/// Circular buffer implemented as a fixed, heap-allocated array.
+/// When full, the next incoming element will take the place of the oldest one.
+/// Not thread-safe.
+template <typename T>
+class CircularBuffer {
+ public:
+  explicit CircularBuffer(int capacity)
+      : data_(std::make_unique_for_overwrite<T[]>(capacity)), capacity_(capacity) {}
+
+  bool empty() const { return size_ <= 0; }
+  bool full() const { return size_ >= capacity_; }
+  const T* data() const { return data_.get(); }
+  T* data() { return data_.get(); }
+  int capacity() const { return capacity_; }
+  int size() const { return size_; }
+
+  const T& operator[](int i) const { return data_[RealIndex(i)]; }
+  T& operator[](int i) { return data_[RealIndex(i)]; }
+
+  const T& front() const { return data_[front_i_]; }
+  T& front() { return data_[front_i_]; }
+  const T& back() const { return data_[RealIndex(-1)]; }
+  T& back() { return data_[RealIndex(-1)]; }
+
+  T pop_front() {
+    T popped{std::move(data_[front_i_])};
+    IncFront();
+    return popped;
+  }
+
+  void push_back(const T& elem) {
+    data_[back_i_] = elem;
+    IncBack();
+  }
+  void push_back(T&& elem) {
+    data_[back_i_] = std::forward<T>(elem);
+    IncBack();
+  }
+
+  template <typename... Args>
+  T& emplace_back(Args&&... args) {
+    if (full()) {
+      (data_.get() + back_i_)->~T();
+    }
+    T* elem = new (data_.get() + back_i_) T(std::forward<Args>(args)...);
+    IncBack();
+    return *elem;
+  }
+
+  T& recycle_back() {
+    T& elem = data_[back_i_];
+    IncBack();
+    return elem;
+  }
+
+  void Dump(bool dump_data = true) {
+    fmt::print("Circular{{m={} n={} f={} b={}", capacity_, size_, front_i_, back_i_);
+    if (dump_data) {
+      fmt::print(" data={{");
+      for (int i = 0; i < capacity_; i++) {
+        fmt::print("{},", data_[i]);
+      }
+      fmt::print("}}");
+    }
+    fmt::print("}}\n");
+  }
+
+  NOT_COPYABLE_NOR_MOVABLE(CircularBuffer)
+
+ private:
+  std::unique_ptr<T[]> data_;
+  int capacity_;
+  int size_ = 0;
+  int front_i_ = 0;
+  int back_i_ = 0;
+
+  int RealIndex(int i) const noexcept {
+    if (i >= 0) {
+      const int j = front_i_ + i;
+      return j < capacity_ ? j : j - capacity_;
+    } else {
+      const int j = back_i_ + i;
+      return j >= 0 ? j : j + capacity_;
+    }
+  }
+
+  void IncFront() noexcept {
+    if (++front_i_ == capacity_) {
+      front_i_ = 0;
+    }
+    --size_;
+  }
+
+  void IncBack() noexcept {
+    if (++back_i_ == capacity_) {
+      back_i_ = 0;
+    }
+    if (full()) {
+      front_i_ = back_i_;
+    } else {
+      ++size_;
+    }
+  }
+};
---
+++ firmware/main/test/common/test_circular_buffer.cpp
@@ -0,0 +1,116 @@
+// Copyright 2021 summivox. All rights reserved.
+// Authors: summivox@gmail.com
+
+#include "common/circular_buffer.hpp"
+
+#include "unity.h"
+
+TEST_CASE("CircularBuffer - basic integer", "[common]") {
+  CircularBuffer<int> q(5);
+  q.Dump();
+  q.push_back(1);
+  q.push_back(2);
+  q.Dump();
+  q.push_back(3);
+  q.push_back(4);
+  q.Dump();
+  q.push_back(5);
+  q.Dump();
+  TEST_ASSERT(q.full());
+  q.push_back(6);
+  q.Dump();
+  TEST_ASSERT(q.full());
+  TEST_ASSERT(q.front() == 2);
+  TEST_ASSERT(q.back() == 6);
+  TEST_ASSERT(q[0] == 2);
+  TEST_ASSERT(q[1] == 3);
+  TEST_ASSERT(q[2] == 4);
+  TEST_ASSERT(q[3] == 5);
+  TEST_ASSERT(q[4] == 6);
+  TEST_ASSERT(q[-1] == 6);
+  TEST_ASSERT(q[-2] == 5);
+  TEST_ASSERT(q[-3] == 4);
+  TEST_ASSERT(q[-4] == 3);
+  TEST_ASSERT(q[-5] == 2);
+
+  q.emplace_back(7);
+  q.Dump();
+  TEST_ASSERT(q[-1] == 7);
+
+  auto& z = q.recycle_back();
+  z = 8;
+  q.Dump();
+  TEST_ASSERT(q[-1] == 8);
+
+  TEST_ASSERT(q.pop_front() == 4);
+  q.Dump();
+  TEST_ASSERT(q.size() == 4);
+  TEST_ASSERT(q.pop_front() == 5);
+  q.Dump();
+  TEST_ASSERT(q.size() == 3);
+
+  q.push_back(9);
+  q.Dump();
+  TEST_ASSERT(q.size() == 4);
+  q.push_back(10);
+  q.Dump();
+  TEST_ASSERT(q.size() == 5);
+  q.push_back(11);
+  q.Dump();
+  TEST_ASSERT(q.size() == 5);
+  TEST_ASSERT(q.front() == 7);
+}
+
+class A {
+ public:
+  A() { print("A() -- {}\n", x_); }
+  explicit A(double x) : x_(x) { print("A(double) -- {}\n", x_); }
+  A(const A& other) : x_(other.x_) { print("A(const A&) -- {}\n", x_); }
+  A(A&& other) : x_(other.x_) {
+    other.x_ = 1e100;
+    print("A(A&&) -- {}\n", x_);
+  }
+  virtual ~A() { print("~A -- {}\n", x_); }
+  A& operator=(const A& other) {
+    x_ = other.x_;
+    print("A& operator=(const A&) -- {}\n", x_);
+    return *this;
+  }
+  A& operator=(A&& other) {
+    x_ = other.x_;
+    other.x_ = 2e100;
+    print("A& operator=(A&& other) -- {}\n", x_);
+    return *this;
+  }
+
+  static A FromDouble(double x) { return A{x}; }
+
+  operator double() const { return x_; }
+
+ private:
+  double x_;
+};
+
+void TestClass() {
+  CircularBuffer<A> q(3);
+  q.Dump();
+
+  q.emplace_back(1.1);
+  q.Dump();
+  q.emplace_back(1.2);
+  q.Dump();
+  q.emplace_back(1.3);
+  q.Dump();
+  q.emplace_back(2.1);
+  q.Dump();
+  print("--- push_back ---\n");
+  q.push_back(A::FromDouble(2.2));
+  q.Dump();
+  print("-----\n");
+  A popped = q.pop_front();
+  q.Dump();
+  TEST_ASSERT(double(popped) == 1.3);
+  popped = q.pop_front();
+  q.Dump();
+  TEST_ASSERT(double(popped) == 2.1);
+}
