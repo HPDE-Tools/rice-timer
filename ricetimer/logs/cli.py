a5ea3f795f55eff2664815da27f343985fef4c62
summivox <summivox@gmail.com>
2021-12-14 14:16:23 -0800

    add CAN dumping to CLI (CSV format with interpolated timestamp)

--- ricetimer/logs/cli.py
+++ ricetimer/logs/cli.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from io import TextIOBase
 from pathlib import Path
 
 import click
@@ -6,40 +7,70 @@ import click_spinner
 import toml
 
 from ricetimer import DATA_PATH
-from ricetimer.logs.format import vbo
+from ricetimer.logs import format
 from ricetimer.logs.ingest import ingest_log_lines
 from ricetimer.logs.processing import tabulate_with_profile
 
 
-@click.command()
+@click.group()
+def export():
+    pass
+
+
+@export.command()
 @click.option('-o', '--output', 'output_file', type=click.File(mode='w', encoding='utf8'))
-@click.option('-p', '--profile')
+@click.option('-p', '--profile', 'profile_name')
 @click.argument('src_dir', type=click.Path(exists=True, file_okay=False, path_type=Path))
-def export(src_dir: Path, output_file: Path, profile):
-    profile_path = resolve_profile_path(profile)
+def vbo(src_dir: Path, output_file: Path, profile_name):
+    profile_path = resolve_profile_path(profile_name)
     try:
         profile = toml.load(profile_path)
     except FileNotFoundError:
         click.echo(
-            f'cannot find profile {profile} -- full path: {profile_path}')
+            f'cannot find profile {profile_name} -- full path: {profile_path}')
         profile = None
     except ValueError as e:
-        click.echo(f'error parsing profile {profile}: {e}')
+        click.echo(f'error parsing profile {profile_name}: {e}')
         profile = None
 
+    log_events = load_events(src_dir)
+    click.echo('converting to table...')
+    with click_spinner.spinner():
+        table = tabulate_with_profile(log_events, profile)
+    click.echo('writing output file...')
+    with click_spinner.spinner():
+        format.vbo.write_vbo(table, output_file)
+    click.echo('done!')
+
+
+@export.command()
+@click.option('-o', '--output', 'output_file', type=click.File(mode='w', encoding='utf8'))
+@click.option('-h/-H', '--header/--noheader', 'write_header', default=True)
+@click.argument('src_dir', type=click.Path(exists=True, file_okay=False, path_type=Path))
+def can(src_dir: Path, output_file: TextIOBase, write_header: bool):
+    if write_header:
+        output_file.write('timestamp,id,dlc,data\n')
+    count = 0
+    log_events = load_events(src_dir)
+    for typestr, timestamp, content in log_events:
+        if typestr != 'c':
+            continue
+        id, dlc, data = content
+        # hack: ID within 11-bit range will be considered standard; otherwise extended
+        id_str = f'{id:03X}' if id <= 0x7ff else f'{id:08X}'
+        output_file.write(f'{timestamp.timestamp():.3f},{id_str},{dlc},{data.hex().upper()}\n')
+        count += 1
+    click.echo(f'{count} CAN frames written.')
+
+
+def load_events(src_dir: Path):
     splits = find_log_files(src_dir)
     click.echo(f'found {len(splits)} log files in: {src_dir}')
     click.echo('loading log...')
     with click_spinner.spinner():
         log_events = ingest_log_lines(files_to_lines(splits))
     click.echo(f'{len(log_events)} events loaded.')
-    click.echo('converting to table...')
-    with click_spinner.spinner():
-        table = tabulate_with_profile(log_events, profile)
-    click.echo('writing output file...')
-    with click_spinner.spinner():
-        vbo.write_vbo(table, output_file)
-    click.echo('done!')
+    return log_events
 
 
 def resolve_profile_path(name: str):
